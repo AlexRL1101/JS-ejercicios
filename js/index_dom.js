88212448585bb49d546bd0fa843b6e029e4de75b
charlyRL <charlyleon763@gmail.com>
2022-01-10 12:20:27 -0600

    100 Video Inteligente

--- js/index_dom.js
+++ js/index_dom.js
@@ -13,6 +13,8 @@ import getGeolocation from "./dom/geolocalizacion.js";
 import searchFilters from "./dom/filtro_busquedas.js";
 import draw from "./dom/sorteo.js";
 import slider from "./dom/carrusel.js";
+import scrollSpy from "./dom/scroll_espia.js";
+import smartVideo from "./dom/video_inteligente.js";
 
 const d = document;
 
@@ -37,6 +39,8 @@ d.addEventListener("DOMContentLoaded", (e) => {
     searchFilters(".card-filter", ".card");
     draw("#winner-btn", ".player");
     slider();
+    scrollSpy();
+    smartVideo();
 });
 
 d.addEventListener("keydown", (e) => {
